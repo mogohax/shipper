13ac0434d79335933523aca3e0f6e0e3adb94dca
Modestas Go <mogohax@users.noreply.github.com>
2018-10-28 22:53:30 +0200

    Create user-service

---
+++ user-service/proto/user/extensions.go
@@ -0,0 +1,11 @@
+package go_micro_srv_user
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/satori/go.uuid"
+)
+
+func (model *User) BeforeCreate(scope *gorm.Scope) error {
+	uid := uuid.NewV4()
+	return scope.SetColumn("Id", uid.String())
+}
---
+++ user-service/repository.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	pb "github.com/mogohax/shipper/user-service/proto/user"
+)
+
+type IRepository interface {
+	GetAll() ([]*pb.User, error)
+	Get(id string) (*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmailAndPassword(user *pb.User) (*pb.User, error)
+}
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
+	if err := repo.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
+	var user *pb.User
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (repo *UserRepository) GetByEmailAndPassword(user *pb.User) (*pb.User, error) {
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (repo *UserRepository) Create(user *pb.User) error {
+	if err := repo.db.Create(user).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
