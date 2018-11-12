ebdd491afcf65d503a1b86d2a74d1640534e9a2f
Modestas Go <mogohax@users.noreply.github.com>
2018-11-13 01:28:00 +0200

    Encode & decode JWT

--- user-service/token-service.go
+++ user-service/token-service.go
@@ -1,18 +1,55 @@
 package main
 
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	pb "github.com/mogohax/shipper/user-service/proto/user"
+)
+
+var (
+	// key used as a salt for our token hashing
+	key = []byte("veriverisecretkey")
+)
+
+type CustomClaims struct {
+	User *pb.User
+	jwt.StandardClaims
+}
+
 type Authable interface {
-	Decode(token string) (interface{}, error)
-	Encode(data interface{}) (string, error)
+	Decode(token string) (*CustomClaims, error)
+	Encode(user *pb.User) (string, error)
 }
 
 type TokenService struct {
 	repo IRepository
 }
 
-func (srv *TokenService) Decode(token string) (interface{}, error) {
-	return "", nil
+func (srv *TokenService) Decode(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func (token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
 }
 
-func (srv *TokenService) Encode(data interface{}) (string, error) {
-	return "", nil
+func (srv *TokenService) Encode(user *pb.User) (string, error) {
+	expireToken := time.Now().Add(time.Hour * 72).Unix()
+
+	claims := CustomClaims{
+		user,
+		jwt.StandardClaims{
+			ExpiresAt: expireToken,
+			Issuer: "go.micro.srv.user",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(key)
 }
